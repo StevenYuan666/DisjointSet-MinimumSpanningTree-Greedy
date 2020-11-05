3430670cf60ed24cafea521f32c485db3114427b
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2020-11-06 00:17:14 +0800

    Complete the initial version of Kruskal part

--- A2/src/Kruskal.java
+++ A2/src/Kruskal.java
@@ -6,13 +6,29 @@ public class Kruskal{
 
         /* Fill this method (The statement return null is here only to compile) */
         
-        return null;
+    	//build up an empty result graph
+    	WGraph result = new WGraph();
+    	ArrayList<Edge> increasing = g.listOfEdgesSorted();
+    	DisjointSets components = new DisjointSets(g.getNbNodes());
+    	for(int i = 0; i < increasing.size(); i ++) {
+    		Edge e = increasing.get(i);
+    		if(IsSafe(components, e)) {
+    			int node1 = e.nodes[0];
+    			int node2 = e.nodes[1];
+    			components.union(node1, node2);
+    			result.addEdge(e);
+    		}
+    	}
+    	
+    	return result;
     }
 
     public static Boolean IsSafe(DisjointSets p, Edge e){
 
         /* Fill this method (The statement return 0 is here only to compile) */
-        return true;
+        int node1 = e.nodes[0];
+        int node2 = e.nodes[1];
+        return (p.find(node1) != p.find(node2));
     
     }
 
